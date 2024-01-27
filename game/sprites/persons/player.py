44ba543867987286f4afef761aee7e6be0d866e6
TsayemDaniela <tsadaniela@gmail.com>
2024-01-27 13:32:41 +0100

    Player and Player's Back

--- game/sprites/persons/player.py
+++ game/sprites/persons/player.py
@@ -5,7 +5,7 @@ from utils.loader import load_image
 class Player(Person):
     def __init__(self):
         Person.__init__(self)
-        self.surface, self.rect = load_image("star.png", -1, scale=0.05)
+        self.surface, self.rect = load_image("PlayerL.png", -1, scale=1)
         self.isCaught = False
         self.velocity = 1
         self.rect.topleft = 10, 90
