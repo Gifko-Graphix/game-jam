2b6abf4a43957f77a6aca04a2320a45edf0cfebb
Edwin Agbakpe <edwin.agbakpe@netlight.com>
2024-01-28 13:12:09 +0100

    conv belt

--- game/sprites/environment/conveyor_belt.py
+++ game/sprites/environment/conveyor_belt.py
@@ -1,12 +1,21 @@
 """Conveyor belt sprite class."""
-from pygame import Surface, time
+from pygame import Surface, time, image
 from pygame.sprite import Sprite
 
 from game.defs import ELECTRIC_PANEL_TIMER_EVENT
 from game.levels.parameters import ConveyorBeltParameters
 from game.sprites.environment.electricPanel import OFF_TIME_SECONDS
 from game.sprites.persons.player import Player
+from utils.loader import load_image
 
+
+ConveyorBeltFiles = [
+    "LL5.png",
+    "LL4.png",
+    "LL3.png",
+    "LL2.png",
+    "LL1.png",
+]
 
 class ConveyorBelt(Sprite):
     """Conveyor belt sprite."""
@@ -19,12 +28,19 @@ class ConveyorBelt(Sprite):
         # self.surface.set_colorkey((255, 255, 255), RLEACCEL)
         self.canInteract = False
         self.surface = Surface((500, 50))
-        self.surface.fill((255, 255, 255))
+        self.surfaces = [load_image(file,(0, 0, 0), 0.25)[0] for file in ConveyorBeltFiles]
+        self.surface = load_image("LL5.png",(0, 0, 0), 0.25)[0]
         self.rect = self.surface.get_rect(center=params.position)
         self.direction = params.direction
+        self.animSpeed = 10
+        self.animCount = len(self.surfaces) * self.animSpeed
 
     def update(self) -> None:
         """Update conveyor belt sprite."""
+        if self.animCount <= 0:
+            self.animCount = len(self.surfaces) * self.animSpeed
+        self.surface = self.surfaces[(self.animCount // self.animSpeed) % len(self.surfaces)]
+        self.animCount -= 1
 
     def detectPlayer(self, player: Player) -> None:
         if self.hitbox.colliderect(player.rect):
